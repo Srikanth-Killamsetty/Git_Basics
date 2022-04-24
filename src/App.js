ace10d57f9f1e15c14961526cac51691914dfcd8
srikanthkillamsetty96 <srikanthkillamsetty96@gmail.com>
2022-04-24 19:31:46 +0530

    Created

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ul>
+        <li>
+          <a href="#"><nav>Home</nav></a>
+          <a href="#"><nav>About</nav></a>
+          <a href="#"><nav>Login</nav></a>
+          <a href="#"><nav>Register</nav></a>
+          <a href="#"><nav>Logout</nav></a>
+          <a href="#"><nav>Services</nav></a>
+          <a href="#"><nav>contact Us</nav></a>
+        </li>
+      </ul>
+      <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
+    </div>
+    
+  );
+}
+
+export default App;
