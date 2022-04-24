ad31a65b7247a71c941e83494c2f3113d74c50fe
Srikanth Killamsetty <84588166+Srikanth-Killamsetty@users.noreply.github.com>
2022-04-24 20:13:49 +0530

    Updated the code
    Added H2 tag and removed the duplicate elements

--- src/App.js
+++ src/App.js
@@ -15,29 +15,10 @@ function App() {
           <a href="#"><nav>contact Us</nav></a>
         </li>
       </ul>
-      <ul>
-        <li>
-          <a href="#"><nav>Home</nav></a>
-          <a href="#"><nav>About</nav></a>
-          <a href="#"><nav>Login</nav></a>
-          <a href="#"><nav>Register</nav></a>
-          <a href="#"><nav>Logout</nav></a>
-          <a href="#"><nav>Services</nav></a>
-          <a href="#"><nav>contact Us</nav></a>
-        </li>
-      </ul>
-    <ul>
-        <li>
-          <a href="#"><nav>Home</nav></a>
-          <a href="#"><nav>About</nav></a>
-          <a href="#"><nav>Login</nav></a>
-          <a href="#"><nav>Register</nav></a>
-          <a href="#"><nav>Logout</nav></a>
-          <a href="#"><nav>Services</nav></a>
-          <a href="#"><nav>contact Us</nav></a>
-        </li>
-      </ul>
-      <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
+    <h2>Modified in Github And Pulled Successfully</h2>
+    
+      
+     <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
     </div>
     
   );
