db87d182933c8128870ea4d1532a2a9b4eb2c32c
srikanthkillamsetty96 <srikanthkillamsetty96@gmail.com>
2022-05-18 21:58:53 +0530

    success

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="container">
+      <h1>Total Merging</h1>
       <div className="main">
         <div className="containers">
-        <h1>master</h1>
+          <h1>master</h1>
           <ul>
             <li>
              <a href="#"><nav>Home</nav></a>
@@ -19,35 +20,8 @@ function App() {
           <p>lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adipvvlorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adip</p>   
        </div>
      </div>
-     <div className="main">
-        <div className="containers">
-          <ul>
-            <li>
-             <a href="#"><nav>Home</nav></a>
-             <a href="#"><nav>About</nav></a>
-             <a href="#"><nav>Login</nav></a>
-             <a href="#"><nav>Register</nav></a>
-           </li>
-          </ul>
-          <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
-          <p>lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adipvvlorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adip</p>   
-       </div>
      </div>
-     <div className="main">
-        <div className="containers">
-          <ul>
-            <li>
-             <a href="#"><nav>Home</nav></a>
-             <a href="#"><nav>About</nav></a>
-             <a href="#"><nav>Login</nav></a>
-             <a href="#"><nav>Register</nav></a>
-           </li>
-          </ul>
-          <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
-          <p>lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adipvvlorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adip</p>    
-       </div>
-     </div>
-   </div>
+     
     
   );
 }
