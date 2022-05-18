704ee97a0423490246e23a6fe212e71d1e088aaa
srikanthkillamsetty96 <srikanthkillamsetty96@gmail.com>
2022-05-18 22:14:28 +0530

    success

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-    <h1>stashbranch1</h1>
-      <ul>
-        <li>
-          <a href="#"><nav>Home</nav></a>
-          <a href="#"><nav>About</nav></a>
-          <a href="#"><nav>Login</nav></a>
-          <a href="#"><nav>Register</nav></a>
-          <a href="#"><nav>Logout</nav></a>
-          <a href="#"><nav>Services</nav></a>
-          <a href="#"><nav>contact Us</nav></a>
-        </li>
-      </ul>
-    <h2>Modified in Github And Pulled Successfully</h2>
-    
-      
-     <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
-    </div>
-    
+    <div className="main">
+        <div className="containers">
+          <h1>stash1</h1>
+          <ul>
+            <li>
+             <a href="#"><nav>Home</nav></a>
+             <a href="#"><nav>About</nav></a>
+             <a href="#"><nav>Login</nav></a>
+             <a href="#"><nav>Register</nav></a>
+           </li>
+          </ul>
+          <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
+          <p>lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adipvvlorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adip</p>   
+       </div>
+     </div>
   );
 }
 
