e722b24980a417f8a8bb7cee436c1afec22312a4
srikanthkillamsetty96 <srikanthkillamsetty96@gmail.com>
2022-05-18 22:25:33 +0530

    success

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 
 function App() {
   return (
+    <div className="main">
+        <h1>This is Main Page</h1>
+        <div className="containers">
+          <h1>main</h1>
+          <ul>
+            <li>
+             <a href="#"><nav>Home</nav></a>
+             <a href="#"><nav>About</nav></a>
+             <a href="#"><nav>Login</nav></a>
+             <a href="#"><nav>Register</nav></a>
+           </li>
+          </ul>
+          <img src="https://pbs.twimg.com/media/EyWW1CBWgAIK-VK.jpg"></img>
+          <p>lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adipvvlorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adip</p>   
+       </div>
+     </div>
     
-     
-    
+
   );
 }
 
